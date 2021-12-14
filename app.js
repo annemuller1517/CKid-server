7b9b056bb803618a7be8c7fca9a518f4cdcf5fbe
Anne Muller <anne_muller@live.nl>
2021-12-14 11:41:28 +0100

    first deploy

--- app.js
+++ app.js
@@ -49,6 +49,16 @@ app.use("/api", wordRoutes);
 const profileRoutes = require("./routes/profile.routes");
 app.use("/api", profileRoutes);
 
+const detailRoutes = require("./routes/details.routes");
+app.use("/api", detailRoutes);
+
+app.use((req, res, next) => {
+	// If no routes match, send them the React HTML.
+	res.sendFile(__dirname + "/public/index.html");
+});
+
+
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
---
+++ routes/details.routes.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const Country = require("../models/Country.model");
+
+router.get("/:country/:city/:lat/:lon/details", (req, res, next) => {
+        let {country, city, lat, lon} = req.params
+        console.log(city)
+    
+    
+    Country.findOne({city})
+    .then((response)=> {
+        res.status(200).json(response)
+    })
+    .catch((err)=> {
+        next(err)
+    })
+});
+
+// You put the next routes here 👇
+// example: router.use("/auth", authRoutes)
+
+module.exports = router;
